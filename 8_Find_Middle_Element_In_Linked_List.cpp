a833671e548577ae5d8ba2016a3c4ab2acce2347
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-25 10:40:43 +0530

    8_Find_Middle_Element_In_Linked_List.cpp

---
+++ 8_Find_Middle_Element_In_Linked_List.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        int total_val = 0;
+        Node * ptr = head;
+        while(ptr!=NULL){
+            ptr=ptr->next;
+            total_val++;
+        }
+        int num ;
+    if(total_val%2==0){
+        num = total_val/2 +1;
+    }
+    else{
+        
+        num = (total_val+1)/2;
+    }
+        ptr = head;
+        for(int i=2;i<=num;i++){
+            ptr=ptr->next;
+        }
+        int vals = ptr->data;
+        return vals;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
