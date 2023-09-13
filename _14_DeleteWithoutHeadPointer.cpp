8bb7840eb1a4b796ec5359c01c9b08c206edaafd
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-09-13 19:34:03 +0530

    Update _14_DeleteWithoutHeadPointer.cpp

--- _14_DeleteWithoutHeadPointer.cpp
+++ _14_DeleteWithoutHeadPointer.cpp
@@ -1,1 +1,14 @@
-// hey
+
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+      Node *temp = del->next;
+      del->data = temp->data;
+      del->next=temp->next;
+      delete(temp);
+    }
+
+}
